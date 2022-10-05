e5bd4d9eded9ae3d85d95b2d1da4932ae64e5ad2
Deniss Simonaits <dsimonaits@gmail.com>
2022-10-06 00:04:51 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const refs = {
+  body: document.body,
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
 
+refs.btnStop.disabled = true;
+
+const randomBackgroundColor = {
+  getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  },
+  DELLAY: 1000,
+  intervalID: null,
+  start() {
+    refs.btnStart.addEventListener('click', () => {
+      refs.btnStart.disabled = true;
+      refs.btnStop.disabled = false;
+
+      this.intervalID = setInterval(() => {
+        refs.body.style.backgroundColor = this.getRandomHexColor();
+      }, this.DELLAY);
+    });
+
+    refs.btnStop.addEventListener('click', () => {
+      this.stop();
+    });
+  },
+
+  stop() {
+    refs.btnStart.disabled = false;
+    refs.btnStop.disabled = true;
+    clearInterval(this.intervalID);
+  },
+};
+
+randomBackgroundColor.start();
